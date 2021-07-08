9962483f375f579adbdd011d769278736b61b4ba
liua13 <liua1@bxscience.edu>
2021-07-08 00:48:43 -0400

    add components, data, and styles folders

---
+++ src/components/aboutTTB.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// sections: front, about, schedule, sponsor, get involved, challenges
+
+const aboutTTB = () => {
+    return <div>About TTB</div>
+};
+
+export default aboutTTB;
