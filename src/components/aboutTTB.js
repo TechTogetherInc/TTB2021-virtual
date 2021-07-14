9ef3e8a7099b850a2c2f97988753d1721eb0c2c4
liua13 <liua1@bxscience.edu>
2021-07-14 19:57:41 -0400

    Add basic template

--- src/components/aboutTTB.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-// sections: front, about, schedule, sponsor, get involved, challenges
-
-const aboutTTB = () => {
-    return <div>About TTB</div>
-};
-
-export default aboutTTB;
