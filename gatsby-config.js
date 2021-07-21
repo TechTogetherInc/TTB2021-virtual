efd360d3c8e9049a98cb674aecf3d86c4ecd3128
liua13 <liua1@bxscience.edu>
2021-07-21 19:07:30 -0400

    Add landing + some components

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,5 +12,14 @@ module.exports = {
       },
     },
     "gatsby-plugin-react-helmet",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
   ],
 };
